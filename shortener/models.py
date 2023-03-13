3abb08c97eb9b41fe6fbc0091b94ba84ae51e096
BaeSungHyun <shijr725@naver.com>
2023-03-13 15:32:35 +0900

    'model_abstract_class'

--- shortener/models.py
+++ shortener/models.py
@@ -41,7 +41,7 @@ from django.contrib.auth.models import User
 #     def rand_string():
 #         str_pool = string.digits + string.ascii_letters
 #         return ("".join([random.choice(str_pool) for _ in range(6)]).lower())
-    
+
 #     nick_name = models.CharField(max_length=100)
 #     created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
 #     target_url = models.CharField(max_length=2000)
@@ -50,67 +50,83 @@ from django.contrib.auth.models import User
 #     updated_at = models.DateTimeField(auto_now=True)
 #     created_at = models.DateTimeField(auto_now_add=True)
 
-class PayPlan(models.Model):
+
+# Django 는 이걸 하나의 table 로 인식하기에 class Meta 를 추가해주자
+# 이거는 기존의 것과 같아서 djanogo 가 makemigrations 에서 알아채지 못함!
+class TimeStampedModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class PayPlan(TimeStampedModel):
     name = models.CharField(max_length=32)
     price = models.IntegerField()
-    updated_at = models.DateTimeField(auto_now = True)
-    create_at = models.DateTimeField(auto_now_add=True)
 
-class Organization(models.Model):
+
+class Organization(TimeStampedModel):
     class Industries(models.TextChoices):
         PERSONAL = "personal"
         RETAIL = "retail"
         MANUFACTURING = "manufacturing"
         IT = "it"
         OTHERS = "others"
-    name = models.CharField(max_length=50)
-    industry = models.CharField(max_length=15, choices=Industries.choices, default=Industries.OTHERS)
-    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING, null=True) # gets pay_plan_id
-    updated_at = models.DateTimeField(auto_now = True)
-    create_at = models.DateTimeField(auto_now_add=True)
 
-class Users(models.Model): 
-    user = models.OneToOneField(User, on_delete=models.CASCADE) # AUTH_USER_MODEL 이랑 1:1 mapping. Settings 에서 지워줘야함
+    name = models.CharField(max_length=50)
+    industry = models.CharField(
+        max_length=15, choices=Industries.choices, default=Industries.OTHERS
+    )
+    pay_plan = models.ForeignKey(
+        PayPlan, on_delete=models.DO_NOTHING, null=True
+    )  # gets pay_plan_id
+
+
+class Users(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE
+    )  # AUTH_USER_MODEL 이랑 1:1 mapping. Settings 에서 지워줘야함
     full_name = models.CharField(max_length=100, null=True)
     # organization 만들면 유료, 안 만들면 무료
-    organization = models.ForeignKey(Organization, on_delete=models.DO_NOTHING, null=True) 
+    organization = models.ForeignKey(
+        Organization, on_delete=models.DO_NOTHING, null=True
+    )
 
-class EmailVerification(models.Model):
+
+class EmailVerification(TimeStampedModel):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     key = models.CharField(max_length=100, null=True)
     verified = models.BooleanField(default=False)
-    updated_at = models.DateTimeField(auto_now=True)
-    create_at = models.DateTimeField(auto_now_add=True)
 
-class Categories(models.Model):
+
+class Categories(TimeStampedModel):
     name = models.CharField(max_length=100)
-    organization = models.ForeignKey(Organization, on_delete=models.DO_NOTHING, null=True) # null=True 는 ForeignKey에서는 안좋음
+    organization = models.ForeignKey(
+        Organization, on_delete=models.DO_NOTHING, null=True
+    )  # null=True 는 ForeignKey에서는 안좋음
     # index가 섞인다고 해야되나..?
     creator = models.ForeignKey(Users, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now = True)
-    create_at = models.DateTimeField(auto_now_add=True)
 
-class ShortenedUrls(models.Model):
+
+class ShortenedUrls(TimeStampedModel):
     class UrlCreatedVia(models.TextChoices):
         WEBSITE = "web"
         TELEGRAM = "telegram"
-    
+
     def rand_string():
         str_pool = string.digits + string.ascii_letters
         return ("".join([random.choice(str_pool) for _ in range(6)])).lower()
-    
+
     nick_name = models.CharField(max_length=100)
     category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING, null=True)
-    prefix = models.CharField(max_length=50) # /a/1234, /b/1234 ... 기준을 하나 더 만들어줌. 더 많은 url
+    prefix = models.CharField(
+        max_length=50
+    )  # /a/1234, /b/1234 ... 기준을 하나 더 만들어줌. 더 많은 url
     creator = models.ForeignKey(Users, on_delete=models.CASCADE)
     target_url = models.CharField(max_length=2000)
-    shortend_url = models.CharField(max_length=6, default=rand_string)    
-    create_via = models.CharField(max_length=8, choices=UrlCreatedVia.choices, default=UrlCreatedVia.WEBSITE)
+    shortend_url = models.CharField(max_length=6, default=rand_string)
+    create_via = models.CharField(
+        max_length=8, choices=UrlCreatedVia.choices, default=UrlCreatedVia.WEBSITE
+    )
     expired_at = models.DateTimeField(null=True)
-    updated_at = models.DateTimeField(auto_now = True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-
-
-
